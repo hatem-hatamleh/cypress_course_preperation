a516956e6d3f618b3355f5db52a44ad846e2105f
Hatem Hatamleh <78985254+hatem-hatamleh@users.noreply.github.com>
2022-06-27 14:28:57 +0200

    Update auth.cy.js

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress"/>
 
 import LoginPage from "../pages/LoginPage";
-
+// Add pull request
 describe("Login test cases", () => {
   it("should be able to login using email and password", () => {
     new LoginPage()
